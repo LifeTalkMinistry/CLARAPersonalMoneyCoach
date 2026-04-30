0ca31589fe609216f11b2ce05a1e1a2fc05fb9d1
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-04-30 23:16:45 +0800

    Wrap app with ClaraThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClaraThemeProvider } from './pages/settings/theme/ThemeContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClaraThemeProvider>
+      <App />
+    </ClaraThemeProvider>
   </StrictMode>,
 )
