3b8e757242e76976b695476dd0d6546fdfddcd73
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-01 09:39:33 -0700

    Create BillboardList.jsx

---
+++ src/pages/settings/admin/billboard/BillboardList.jsx
@@ -0,0 +1,7 @@
+export default function BillboardList() {
+  return (
+    <div className="rounded-[24px] border border-white/10 bg-white/[0.045] p-4 text-white">
+      Billboard List (fetch from Supabase)
+    </div>
+  );
+}
