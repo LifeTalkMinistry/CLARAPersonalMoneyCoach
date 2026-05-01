86f62f4cdca0736d2f3c3bb839546fe0b69febc5
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-01 21:11:10 +0800

    Add UserManagement safely

--- src/pages/settings/admin/AdminPanel.jsx
+++ src/pages/settings/admin/AdminPanel.jsx
@@ -1,11 +1,13 @@
 import ClaraPageShell from "../../../components/shared/layout/ClaraPageShell";
 import BillboardManager from "./billboard/BillboardManager";
+import UserManagement from "./users/UserManagement";
 
 export default function AdminPanel() {
   return (
     <ClaraPageShell>
       <div className="space-y-4">
         <BillboardManager />
+        <UserManagement />
       </div>
     </ClaraPageShell>
   );
