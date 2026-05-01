1f68a6b75c38c91aaf2d2a80a44bbeeb622b3157
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-01 07:13:24 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import { ClaraThemeProvider } from './pages/settings/theme/ThemeContext'
-import { AvatarProvider } from './context/AvatarContext'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { ClaraThemeProvider } from "./pages/settings/theme/ThemeContext";
+import { AvatarProvider } from "./context/AvatarContext";
+import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
-      <ClaraThemeProvider>
-        <AvatarProvider>
-          <App />
-        </AvatarProvider>
-      </ClaraThemeProvider>
+      <AuthProvider>
+        <ClaraThemeProvider>
+          <AvatarProvider>
+            <App />
+          </AvatarProvider>
+        </ClaraThemeProvider>
+      </AuthProvider>
     </HashRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
