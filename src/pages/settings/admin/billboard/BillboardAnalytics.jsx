cbfc02a03e5fca4ee07897a10a42126da979c5f0
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-01 09:39:59 -0700

    Create BillboardAnalytics.jsx

---
+++ src/pages/settings/admin/billboard/BillboardAnalytics.jsx
@@ -0,0 +1,7 @@
+export default function BillboardAnalytics() {
+  return (
+    <div className="rounded-[24px] border border-white/10 bg-white/[0.045] p-4 text-white">
+      Billboard Analytics (views / clicks)
+    </div>
+  );
+}
