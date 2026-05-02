73c089f6be2673b54ef51bdc315a2594598fb9be
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-02 11:10:54 +0800

    Add blank HomeV2 page

---
+++ src/pages/HomeV2.jsx
@@ -0,0 +1,9 @@
+import ClaraPageShell from "../components/shared/layout/ClaraPageShell";
+
+export default function HomeV2() {
+  return (
+    <ClaraPageShell compactHeader>
+      <div className="min-h-[100dvh] px-3 pb-24" />
+    </ClaraPageShell>
+  );
+}
