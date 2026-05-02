e78201f293ba6653eab1aef61bcce0aa319105e0
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-02 11:09:00 +0800

    Add Home V2 route back

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
+import HomeV2 from "./pages/HomeV2";
 import Feed from "./pages/Feed";
 import Messages from "./pages/Messages";
 import Settings from "./pages/settings/Settings";
@@ -50,6 +51,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/home-v2" element={<HomeV2 />} />
       <Route path="/feed" element={<Feed />} />
       <Route path="/messages" element={<Messages />} />
       <Route path="/settings" element={<Settings />} />
